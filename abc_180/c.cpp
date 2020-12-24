0351a46465c4c8818b320be66fc5cea0379eeb2c
dashimaki360 <17.punch@gmail.com>
2020-12-24 16:09:23 +0900

    update

--- abc_180/c.cpp
+++ abc_180/c.cpp
@@ -7,5 +7,13 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  ll n;
+  cin >> n;
+
+  rep(i, (int)sqrt(n)+1){
+    if(i==0) continue;
+    if(n%i==0) cout << i << endl;
+  }
+    cout << n << endl;
   return 0;
 }
