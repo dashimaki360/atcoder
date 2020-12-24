d5f86f41469f587467e93a2654710e3e943fc951
dashimaki360 <17.punch@gmail.com>
2020-12-24 21:05:34 +0900

    update

--- abc_179/f.cpp
+++ abc_179/f.cpp
@@ -7,14 +7,5 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
-  ll x, y, a, b;
-  cin >> x >> y >> a >> b;
-  ll exp=0;
-  while ((double)x*a <= 2e18 && a*x<=x+b && a*x<y) {
-      x *= a;
-      exp++;
-  }
-  exp += (y-x-1)/b;
-  cout << exp << endl;
   return 0;
 }
