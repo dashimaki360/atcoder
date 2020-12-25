9311f281527d7565306b4cfb0775fe58937c866e
dashimaki360 <17.punch@gmail.com>
2020-12-25 13:49:57 +0900

    update

--- abc_180/e.cpp
+++ abc_180/e.cpp
@@ -6,6 +6,35 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
+void chmin(int& x, int y){x = min(x, y);}
+const int INF = 1001001001;
+
 int main() {
+  int n;
+  cin >> n;
+  vector<int> x(n), y(n), z(n);
+  rep(i, n) cin >> x[i] >> y[i] >> z[i];
+  int n2 = 1<<n;
+  vector<vector<int>> dp(n2, vector<int> (n,INF));
+  vector<vector<int>> dist(n, vector<int> (n));
+
+  rep(i,n) rep(j,n){
+    int now = abs(x[j] - x[i]);
+    now += abs(y[j] - y[i]);
+    now += max(0, z[j] - z[i]);
+    dist[i][j] = now;
+  }
+  rep(i,n){
+    if(i==0) continue;
+    dp[1<<i][i] = dist[0][i];
+  }
+  rep(i,n2) rep(j,n){
+    if(~i>>j&1) continue;
+    rep(k,n){
+      if(i>>k&1) continue;
+      chmin(dp[i|1<<k][k], dp[i][j] + dist[j][k]); 
+    }
+  }
+  cout << dp[n2-1][0] << endl;
   return 0;
 }
