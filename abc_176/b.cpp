9ee23a1eebe64dcb5eb82e88b007213ddc236047
dashimaki360 <17.punch@gmail.com>
2020-12-27 12:58:01 +0900

    update

--- abc_176/b.cpp
+++ abc_176/b.cpp
@@ -6,17 +6,6 @@ using namespace std;
 using ll = long long;
 using P = pair<int,int>;
 
-const int N = 1000000007;
 int main() {
-  ll n;
-  cin >> n;
-  ll a=1;
-  ll b=1;
-  ll c=1;
-  rep(i,n) a = (a*10)%N;
-  rep(i,n) b = (b*9)%N;
-  rep(i,n) c = (c*8)%N;
-  ll ans = ((a-2*b+c)%N+N)%N;
-  cout << ans << endl;
   return 0;
 }
