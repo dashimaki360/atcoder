ca3fb89afcbd33224aaf686939b2af3030bd902f
dashimaki360 <17.punch@gmail.com>
2020-12-30 01:25:47 +0900

    update

--- abc_174/e.cpp
+++ abc_174/e.cpp
@@ -7,5 +7,26 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(n);
+  rep(i,n) cin >> a[i];
+
+  int ir = 0;
+  int il = 1e9;
+  while((il-ir) > 1){
+    int x = (il+ir)/2;
+    int tot = 0;
+    rep(i,n){
+      tot += (a[i]-1)/x;
+    }
+    // cout << tot << ", " << il << ", " << ir << endl;
+    if(tot > k){
+      ir = x;
+    }else{
+      il = x;
+    }
+  }
+  cout << il << endl;
   return 0;
 }
