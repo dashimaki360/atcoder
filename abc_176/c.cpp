749092fa240a86c46ecd500fe8d263218604d8e6
dashimaki360 <17.punch@gmail.com>
2021-01-06 03:48:17 +0900

    update

--- abc_176/c.cpp
+++ abc_176/c.cpp
@@ -7,5 +7,18 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+  int n;
+  cin >> n;
+  int me=0, you=0;
+  ll ans=0;
+  rep(i,n){
+    cin >> me;
+    if(me < you){
+      ans += you - me;
+    }else{
+      you = me;
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
