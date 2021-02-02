b408bc452383f50b46f1e7fdb7b9e82a41501597
dashimaki360 <17.punch@gmail.com>
2021-02-02 14:21:13 +0900

    update

--- abc_190/e2.cpp
+++ abc_190/e2.cpp
@@ -11,59 +11,62 @@ void chmax(int& x, int y){x = max(x,y);}
 int main() {
   int n, m;
   cin >> n >> m;
-  vector<vector<int>> to(n);
+  vector<vector<int>> road(n);
   rep(i,m){
     int a, b;
     cin >> a >> b;
     --a; --b;
-    to[a].push_back(b);
-    to[b].push_back(a);
+    road[a].push_back(b);
+    road[b].push_back(a);
   }
   int k;
   cin >> k;
   vector<int> c(k);
-  rep(i,k){
+  rep(i,k) {
     cin >> c[i];
-    c[i]--;
+    --c[i];
   }
-  
-  vector<vector<int>> dist(k, vector<int>(k));
+
   const int INF = 1001001001;
   auto bfs = [&](int sv) {
-    vector<int> dist(n, INF);
+    vector<int> d(n, INF);
     queue<int> q;
-    dist[sv] = 0;
+    d[sv] = 0;
     q.push(sv);
     while (!q.empty()){
       int v = q.front(); q.pop();
-      for(int u: to[v]){
-        if(dist[u] != INF) continue;
-        dist[u] = dist[v]+1;
+      for(int u: road[v]){
+        if(d[u] != INF) continue;
+        d[u] = d[v]+1;
         q.push(u);
       }
     }
-    return dist;
+    return d;
   };
+
+  vector<vector<int>> dist(k, vector<int>(k));
   rep(i,k){
+    // transrate n index > k index
     vector<int> d = bfs(c[i]);
     rep(j,k) dist[i][j] = d[c[j]];
   }
 
-  int k2 = 1<<k;
-  vector dp(k2, vector<int>(k,INF));
+  ll k2 = 1<<k;
+  vector dp(k2, vector<int>(k, INF));
   rep(i,k) dp[1<<i][i] = 1;
-  rep(s,k2) rep(i,k){
-    if(~s>>i&1) continue;
-    rep(j,k){
-      if(s>>j&1) continue;
-      chmin(dp[s|1<<j][j], dp[s][i] + dist[i][j]);
+  rep(i, k2) rep(j,k){
+    if(~i>>j&1) continue;
+    rep(x,k){
+      if(i>>x&1) continue;
+      chmin(dp[i|1<<x][x], dp[i][j] + dist[j][x]);
     }
   }
 
   int ans = INF;
-  rep(i,k) chmin(ans, dp[k2-1][i]); 
+  rep(i,k){
+    chmin(ans, dp[k2-1][i]);
+  }
   if(ans == INF) ans = -1;
-
   cout << ans << endl;
   return 0;
 }
--- abc_@@/b.cpp
+++ abc_@@/b.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 using ll = long long;
 using P = pair<int,int>;
+void chmin(int& x, int y){x = min(x,y);}
+void chmax(int& x, int y){x = max(x,y);}
 
 int main() {
   return 0;
